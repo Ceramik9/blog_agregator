57024511419ff46bc7b78b3c619662587a95289a
Ceramik9 <grad.sebastian@gmail.com>
2026-09-23 22:21:12 +0100

    added posts table and updated feed fetching to save posts to database

--- feed_scraper.go
+++ feed_scraper.go
@@ -5,7 +5,9 @@ import (
 	"time"
 	"context"
 	"database/sql"
+	"github.com/google/uuid"
 	"github.com/Ceramik9/blog_agregator/internal/database"
+	"strings"
 )
 
 func scrapeFeeds(s *state) error {
@@ -35,13 +37,48 @@ func scrapeFeeds(s *state) error {
 	}
 
 	// fetch feed
-	feed, err := fetchFeed(ctx, feedToScrape.Url.String)
+	rssFeed, err := fetchFeed(ctx, feedToScrape.Url.String)
 	if err != nil {
 		return err
 	}
 	
+	// save posts
+	for _, item := range rssFeed.Channel.Item {
+		var publishedAt sql.NullTime
+		parsedTime, err := time.Parse(time.RFC1123, item.PubDate)
+		if err == nil {
+			publishedAt = sql.NullTime {
+				Time: 	parsedTime,
+				Valid: true,
+			}
+		}
+		now := time.Now()
+		postParams := database.CreatePostParams {
+			ID:          uuid.New(),
+			CreatedAt:   now,
+			UpdatedAt:   now,
+			Title:       item.Title,
+			Url:         item.Link,
+			Description: sql.NullString {
+				String: item.Description,
+				Valid:  true,
+			},
+			PublishedAt: publishedAt,
+			FeedID:      feedToScrape.ID,
+		}
+		_, err = s.db.CreatePost(ctx, postParams)
+		if err != nil {
+			if strings.Contains(err.Error(), "unique constraint") {
+				continue
+			} else {
+				return fmt.Errorf("error saving post:\n%w", err)
+			}
+		}
+	}
+
+	// remove later
 	// print all feed titles
-	for i, item := range feed.Channel.Item {
+	for i, item := range rssFeed.Channel.Item {
 		fmt.Printf("%d. %s\n", i+1, item.Title)
 	}
 	fmt.Println("===================================================================================")
